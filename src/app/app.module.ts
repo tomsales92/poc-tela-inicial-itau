43fd440af9feb8ca9b03d4241e698534df7c7cec
Tom Sales <everton.goncalves92@gmail.com>
2023-09-14 10:51:49 -0300

    configuração inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PronampeComponent } from './pages/pronampe/pronampe.component';
+import { FacilidadesItauComponent } from './pages/facilidades-itau/facilidades-itau.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    PronampeComponent,
+    FacilidadesItauComponent
   ],
   imports: [
     BrowserModule,
